e2882a5c9caa396c73d0e80fd52f65f5a99a8809
Nick Perrin <nick.perrin@lcecorp.com>
2020-01-18 12:04:06 -0600

    added getting lit

--- src/stores/galaStore.js
+++ src/stores/galaStore.js
@@ -28,11 +28,32 @@ const togglePawpurrazzi = (rowIndex, colIndex) => (gala) => {
   return galaCopy;
 };
 
+const validIndicies = (rowIndex, colIndex) => (gala) => {
+  const isIndexValid = (max) => (value) => value >= 0 && value < max;
+  return isIndexValid(gala.length)(rowIndex) && isIndexValid(gala[rowIndex].length)(colIndex);
+}
+
+const tilesUntilWall = (directionalFn) => (tile) => (gala) =>{
+  const checkNext = ([rowIndex, colIndex]) => {
+    if(validIndicies(rowIndex, colIndex)(gala) && !gala[rowIndex][colIndex].isWall) {
+      return [gala[rowIndex][colIndex], ...checkNext(directionalFn([rowIndex, colIndex]))];
+    } else {
+      return [];
+    }
+  }
+  return checkNext(directionalFn([tile.rowIndex, tile.colIndex])).filter(x => x !== undefined)
+}
+
+const tilesNorth = tilesUntilWall(([ri, ci]) => [ri - 1, ci]);
+const tilesSouth = tilesUntilWall(([ri, ci]) => [ri + 1, ci]);
+const tilesEast = tilesUntilWall(([ri, ci]) => [ri, ci + 1]);
+const tilesWest = tilesUntilWall(([ri, ci]) => [ri, ci - 1]);
+
 const illuminationSources = tile => gala => ({
-  north: true,
-  south: true,
-  east: true,
-  west: true,
+  north: some(x => x.hasPawpurrazzi)(tilesNorth(tile)(gala)),
+  south: some(x => x.hasPawpurrazzi)(tilesSouth(tile)(gala)),
+  east: some(x => x.hasPawpurrazzi)(tilesEast(tile)(gala)),
+  west: some(x => x.hasPawpurrazzi)(tilesWest(tile)(gala)),
 })
 
 const lit = (illuminationSources) => (tile) => ({
